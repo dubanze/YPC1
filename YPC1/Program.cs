6cf18a12d790ffbb1e2bac83d85c4cc437b12eaf
Wojciech Dubanowicz <dubanze@gmail.com>
2018-10-24 00:09:23 +0200

    Add project files.

---
+++ YPC1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace YPC1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new Dog();
+            //var animalSound = (IAnimalLegs) animal;
+            Console.WriteLine(animal.GiveVoice());
+            Console.WriteLine(animal.Number);
+        }
+
+        public interface IAnimalSound
+        {
+            string GiveVoice();
+        }
+
+        public interface IAnimalLegs
+        {
+            int Number { get; }
+        }
+
+        public class Duck : IAnimalSound, IAnimalLegs
+        {
+            public string GiveVoice()
+            {
+                return "Quack";
+            }
+
+            public int Number => 2;
+        }
+
+        public class Dog : IAnimalSound, IAnimalLegs
+        {
+            public string GiveVoice()
+            {
+                return "Woof";
+            }
+
+            public int Number => 4;
+        }
+        public class Cat : IAnimalSound
+        {
+            public string GiveVoice()
+            {
+                return "Meow";
+            }
+        }
+
+    }
+    
+}
